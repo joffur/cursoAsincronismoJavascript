3e989003f0ad6b89b2013d2752c080a2a2bce171
Jose Fernando Fuentes Roa <ing.josefuentes@hotmail.com>
2023-06-30 06:25:11 -0500

    Clase - Qué son los Callbacks

---
+++ src/callback/index.js
@@ -0,0 +1,21 @@
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(calc(2, 2, sum));
+
+console.log('Start');
+setTimeout(() => {
+  console.log('Hola JavaScript!');
+}, 5000);
+console.log('After timeout');
+
+function greeting(name, age) {
+  console.log(`Hola ${name}, your age is ${age}!`);
+}
+
+setTimeout(greeting, 2000, 'José', 34);
---
+++ src/callback/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+function greeting(name, age) {
+  console.log(`Hola ${name}, your age is ${age}!`);
+}
+
+setTimeout(greeting, 2000, 'José', 34);
